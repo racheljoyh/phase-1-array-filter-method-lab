ba234d8d2ec9ca2f8c8cbe788befc571d93776f8
Rachel <racheljhumes@gmail.com>
2022-11-04 16:12:25 -0600

    commit tree e80967733fc4077da332a73381d2dabf4f830069

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+
+function findMatching (drivers, name) {
+  return drivers.filter( (possibleMatch) => (possibleMatch.toLowerCase() === name.toLowerCase()) 
+  )
+}
+
+function fuzzyMatch (drivers, string) {
+  return drivers.filter((possibleMatch) => possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0 );
+}
+
+function matchName (drivers, newString) {
+  return drivers.filter((possibleMatch) => possibleMatch.name === newString);
+}
